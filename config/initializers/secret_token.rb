37a307479e31e1677aa8ff0940d94c20aa0993d2
lote <your.email@example.com>
2013-10-12 01:16:41 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visit::Application.config.secret_token = 'c32e6e1cc1f5cc0b34dd904417c17c4bb1f013eb3a60127e750f7872e1e1dce0f8a9f4b77f0155183212e09dc499edfd0ef1d59c6033b9793746bbfe6fe09214'
